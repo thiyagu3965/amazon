39156869b24a4d906f5c5d49d9644482e73476ea
Thiyagu0052 <thiyagu.offm@gmail.com>
2021-06-16 21:06:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import react from 'react'
+import data from './data'
+
+function App() {
+  return (
+
+    <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="index.html">amazon</a>
+          </div>
+          <div>
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <div className="row center">
+              {
+                  data.products.map(product =>(
+                    <div key={product._id} className="card">
+                    <a href={`product/${product._id}`}><img className="medium" src={product.image} alt={product.name} /></a>
+                    <div className="card-body">
+                        <a href={`product/${product._id}`}><h2>{product.name}</h2></a>
+                        <div className="rating">
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star-half-o"></i></span>
+                        </div>
+                         <div className="price"><i className="fa fa-inr" />{product.price}</div>
+                     </div>
+                 </div>
+                  ))
+              }
+            
+             
+           </div>
+        </main>
+        <footer className="row center">All right reserved</footer>
+    </div>
+
+
+    
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products : [
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/1.jpg',
+            price:350,
+            brand:'Nike',
+            rating:'4.5',
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'2',
+            name:'Nike Slim Phants',
+            category:'Phants',
+            image:'/images/1.jpg',
+            price:280,
+            brand:'Nike',
+            rating:'4.5',
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'3',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/1.jpg',
+            price:456,
+            brand:'Nike',
+            rating:'4.5',
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'4',
+            name:'Nike Slim Pants',
+            category:'Pants',
+            image:'/images/1.jpg',
+            price:344,
+            brand:'Nike',
+            rating:'4.5',
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'5',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/1.jpg',
+            price:789,
+            brand:'Nike',
+            rating:'4.5',
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'6',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/1.jpg',
+            price:234,
+            brand:'Nike',
+            rating:'4.5',
+            numReviews:10,
+            description:'high quality product'
+        },
+    ]
+}
+
+export default data;
